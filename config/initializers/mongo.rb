a4470d5ae46af35beec1c0f83b98cc7b2d460854
Josh Walton <jwalton922@gmail.com>
2012-11-14 23:40:10 -0700

    removed openshift code

--- config/initializers/mongo.rb
+++
@@ -1,9 +0,0 @@
-#MongoMapper.connection = Mongo::Connection.new('ds037907.mongolab.com', 37907)
-MongoMapper.connection = Mongo::Connection.new(ENV['OPENSHIFT_MONGODB_DB_HOST'], ENV['OPENSHIFT_MONGODB_DB_PORT'])
-MongoMapper.database = "#myapp-#{Rails.env}"
-
-if defined?(PhusionPassenger)
-   PhusionPassenger.on_event(:starting_worker_process) do |forked|
-     MongoMapper.connection.connect if forked
-   end
-end

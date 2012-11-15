3ae60ac3b2ce7504bcc639582ba8546c15c17aa4
Curtis Allen <curtis.n.allen@gmail.com>
2012-11-14 20:15:06 -0700

    Add select2 and angularui

--- public/js/app.js
+++ public/js/app.js
@@ -2,5 +2,5 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('diabloApp', ['diabloApp.filters', 'diabloApp.services', 'diabloApp.directives']);//.
+angular.module('diabloApp', ['diabloApp.filters', 'diabloApp.services', 'diabloApp.directives', 'ui']);//.
   
